a161ecd8c510eca49c66c744e5e53a6639357dc9
rtfp0102 <rtfp0102@gmail.com>
2019-10-24 14:06:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash = {}
+  data.each do |color_gender_lives, values|
+    values.each do |value, array|
+      array.each do |name|
+        if hash[name] == nil
+          hash[name] = {}
+          hash[name][color_gender_lives] = []
+        else
+          hash[name][color_gender_lives] = []
+        end
+      end
+    end
+  end
+  hash.each do |name, values|
+    values.each do |hashvalue, array|
+      data.each do |color_gender_lives, values|
+        values.each do |value, array|
+
+          array.each do |element|
+            if element == name && hashvalue == color_gender_lives
+
+              hash[name][hashvalue] << value.to_s
+            end
+          end
+        end
+      end
+    end
+  end
+  hash
 end
